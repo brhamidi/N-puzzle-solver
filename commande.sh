ba131a4b1f0c6bf4d746296a998339331eaf04d2
Matthias Srun <matthias.srun@gmail.com>
2019-03-28 12:17:50 +0100

    wip

--- commande.sh
+++
@@ -1,1 +0,0 @@
-clang++ test.cpp  -std=c++11 -I ~/.brew/include/opencv4 -lopencv_core -lopencv_imgcodecs -lopencv_highgui -L ~/.brew/lib
---
+++ src/commande.sh
@@ -0,0 +1,1 @@
+clang++ Graphic_displayer.cpp  -std=c++11 -I ~/.brew/include -I ~/.brew/include/opencv4 -lopencv_core -lopencv_imgcodecs -lopencv_highgui -lopencv_imgproc -lsfml-system -lsfml-window -lsfml-graphics -L ~/.brew/lib main.cpp Solver.cpp
